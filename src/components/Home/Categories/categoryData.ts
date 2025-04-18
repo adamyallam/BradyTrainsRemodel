f06e95edb85c1732115c1553884aa4c332f35d5d
Adam Allam <adamyallam@gmail.com>
2025-04-18 16:59:07 -0500

    Update category images

--- src/components/Home/Categories/categoryData.ts
+++ src/components/Home/Categories/categoryData.ts
@@ -1,43 +1,38 @@
 const data = [
   {
-    title: "Televisions",
+    title: "O Gauge / Scale",
     id: 1,
     img: "/images/categories/categories-01.png",
   },
   {
-    title: "Laptop & PC",
+    title: "HO Scale",
     id: 2,
     img: "/images/categories/categories-02.png",
   },
   {
-    title: "Mobile & Tablets",
+    title: "N Scale",
     id: 3,
     img: "/images/categories/categories-03.png",
   },
   {
-    title: "Games & Videos",
+    title: "New This Week",
     id: 4,
     img: "/images/categories/categories-04.png",
   },
   {
-    title: "Home Appliances",
+    title: "O Gauge Sets",
     id: 5,
     img: "/images/categories/categories-05.png",
   },
   {
-    title: "Health & Sports",
+    title: "All Engines",
     id: 6,
     img: "/images/categories/categories-06.png",
   },
   {
-    title: "Watches",
+    title: "Polar Express Trains",
     id: 7,
     img: "/images/categories/categories-07.png",
-  },
-  {
-    title: "Televisions",
-    id: 8,
-    img: "/images/categories/categories-04.png",
   },
 ];
 
