d3814a086bc8e9a3235701484bc13746155db148
Adam Allam <adamyallam@gmail.com>
2025-05-02 15:42:40 -0500

    added page routes

---
+++ src/app/(site)/(pages)/(shop)/shop-train-sets/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ShopAll from "@/components/ShopAll";
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Shop Page | NextCommerce Nextjs E-commerce template",
+  description: "This is Shop Page for NextCommerce Template",
+  // other metadata
+};
+
+const ShopWithSidebarPage = () => {
+  return (
+    <main>
+      <ShopAll />
+    </main>
+  );
+};
+
+export default ShopWithSidebarPage;
