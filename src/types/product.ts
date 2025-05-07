e47facde69179cef0b39588c010b4346d4ba11db
Adam Allam <adamyallam@gmail.com>
2025-05-07 14:32:41 -0500

    Added new options to manufacturer, scale, and cateogry field

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,9 +1,9 @@
 export type Product = {
   title: string;
   description: string;
-  manufacturer: "Atlas" | "Bachmann" | "MTH" | "Lionel";
-  scale: "N" | "HO" | "O" | "G" | "Z";
-  category: "Locomotives" | "Rolling Stock" | "Accessories" | "Scenery" | "Electronics";
+  manufacturer: "Atlas" | "Bachmann" | "MTH" | "Lionel" | "All Brands";
+  scale: "N" | "HO" | "O" | "G" | "Z" | "S" | "All Scales";
+  category: "Locomotives" | "Rolling Stock" | "Accessories" | "Scenery" | "Electronics" | "All Categories";
   collection?: string;
   reviews: number;
   price: number;
